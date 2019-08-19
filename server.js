7eb38f1dab4872bf6142917bde30ddcc5e370521
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-08-19 18:22:47 -0300

    criando o basico de um sistema node.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const app = require('./src/app.js');
+const debug = require('debug')('james:server');
+const http = require('http');
+
+const port = process.env.port || '3000';
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onLinstening);
+
+console.log('API rodando na porta: ' + port);
+
+function onError(error) {
+    console.log('error: '+ onError);
+
+
+}
+
+function onLinstening(lister) {
+    console.log('onLinstening: ' + lister);
+
+}
