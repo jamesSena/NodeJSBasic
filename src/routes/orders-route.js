691ff7712a8ec8ccf67e26d54ebc68a86b6a9c23
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-09-16 12:46:09 -0300

    Correção na rota de orders

--- src/routes/orders-route.js
+++ src/routes/orders-route.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/orders-controller');
-const authSerivce = require('../services/auth-service');
+const authService = require('../services/auth-service');
 
-router.get('/', authSerivce, controller.get);
-router.post('/', authSerivce, controller.post);
+router.get('/', authService.authorize, controller.get);
+router.post('/', authService.authorize, controller.post);
 
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
