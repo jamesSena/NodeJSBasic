f6f44f77c0fefbd7449cdf83c89a4382bfae2c4d
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-09-16 12:55:25 -0300

    correção no cross

--- src/app.js
+++ src/app.js
@@ -22,7 +22,24 @@ app.use(bodyParser.json({
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Accept,x-access-token');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 const indexRoute = require('./routes/index-route'); //carrega as Rotas
 const productsRoute = require('./routes/products-route'); //carrega as Rotas
 const customersRoute = require('./routes/customer-route'); //carrega as Rotas
